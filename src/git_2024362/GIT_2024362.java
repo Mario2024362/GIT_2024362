4ea32bae158ef7f34ecee9cc22502ea07cbb4372
Mario2024362 <2024362@student.cct.ie>
2024-12-30 13:49:33 +0000

    added a simple print

--- src/git_2024362/GIT_2024362.java
+++ src/git_2024362/GIT_2024362.java
@@ -14,7 +14,8 @@ public class GIT_2024362 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        
+        System.out.println("Mario - 2024362");
+    }   
+
 }
